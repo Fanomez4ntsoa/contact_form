a03a1687df3fdd63ea51ece289245e78c044862b
Fanomez4ntsoa <voary.randrianarison@esti.mg>
2022-05-30 22:10:18 +0300

    Finalization of the project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//obtenons tout les éléments requis
+//Récupérons tout les éléments requis
 
 const form = document.querySelector("form"),
 statusTxt = form.querySelector(".button-area span");
@@ -6,6 +6,7 @@ statusTxt = form.querySelector(".button-area span");
 form.onsubmit = (e) => {
 	e.preventDefault(); //empêche le formulaire de se soumettre
 	statusTxt.style.display = "block";
+	statusTxt.style.color = "darkslategray";
 
 	//Ajax pour envoyer les données du formulaire de html à php
 	let xhr = new XMLHttpRequest(); //créer un nouvel objet xml
@@ -14,11 +15,19 @@ form.onsubmit = (e) => {
 		//une fois le chargement ajax
 		if(xhr.readyState == 4 && xhr.status == 200) { //si le statut de la réponse ajax est 200 et readyState est 4, cela signifie qu'il n'y a pas d'erreur
 			let response = xhr.response; //stockage de la réponse ajax dans une variable
-			console.log(response);
-
+			//si la réponse est une erreur comme entrer une adresse email valide
+			if (response.indexOf("Les champs Email et message sont obligatoires !") != -1 || response.indexOf("Saisissez une email valide !") || response.indexOf("Erreur d'envoye du message !") ) {
+				statusTxt.style.color = "red";
+			} else {
+				form.reset();
+				setTimeout(() => {
+					statusTxt.style.display = "none";
+				}, 3000); //cacher le texte d'état après 3secondes
+			}
+			statusTxt.innerText = response;
 		}
 	}
 	let formData = new FormData(form); //création d'un nouvel objet FormData. 
-																 //Cet objet est utilisé pour envoyer les données du formulaire
+																 		 //Cet objet est utilisé pour envoyer les données du formulaire
 	xhr.send(formData); //envoi de données de formulaire
 } 
--- php/message.php
+++ php/message.php
@@ -1,6 +1,29 @@
 <?php 
 
-	echo "This msg is sent from PHP file";
+	//Récupérations de toutes les valeurs du formulaire
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$website = $_POST['website'];
+	$message = $_POST['message'];
 
+	if(!empty($email) && !empty($message)) { //si les champs "email" et "message" ne sont pas vides
+		if(filter_var($email, FILTER_VALIDATE_EMAIL)) { //si l'email saisi par l'utilisateur est valide
+			$receiver = "voary.fanomezantsoa@gmail.com" //adresse email du destinataire de l'e-mail
+			$subject = "From: $name <$email>" // l'object de l'email.
+			//fusionner toutes les valeurs de l'utilisateur dans la variable. \n est utilisé pour une nouvelle ligne
+			$body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage: $message\n\nRegards,\n$name";
+			$sender = "From: $email;" //email de l'expéditeur
+			if(mail($receiver, $subject, $body, $sender)) { //mail() est une fonction php intégrée qui permet d'envoyer du courrier.
+				echo "Votre message a été envoyé";
+			} else {
+				echo "Erreur d'envoye du message !";
+			}
+		} else {
+			echo "Saisissez une email valide !";
+		}
+	} else {
+		echo "Les champs Email et message sont obligatoires !";
+	}
 
 ?>
