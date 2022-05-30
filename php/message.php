b7697d9dff73dfa2822430598aac6617aa77622f
Fanomez4ntsoa <voary.randrianarison@esti.mg>
2022-05-30 22:28:15 +0300

    Finalization of the project

--- php/message.php
+++ php/message.php
@@ -9,11 +9,11 @@
 
 	if(!empty($email) && !empty($message)) { //si les champs "email" et "message" ne sont pas vides
 		if(filter_var($email, FILTER_VALIDATE_EMAIL)) { //si l'email saisi par l'utilisateur est valide
-			$receiver = "voary.fanomezantsoa@gmail.com" //adresse email du destinataire de l'e-mail
-			$subject = "From: $name <$email>" // l'object de l'email.
+			$receiver = "voary.fanomezantsoa@gmail.com"; //adresse email du destinataire de l'e-mail
+			$subject = "From: $name <$email>"; // l'object de l'email.
 			//fusionner toutes les valeurs de l'utilisateur dans la variable. \n est utilisé pour une nouvelle ligne
 			$body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage: $message\n\nRegards,\n$name";
-			$sender = "From: $email;" //email de l'expéditeur
+			$sender = "From: $email"; //email de l'expéditeur
 			if(mail($receiver, $subject, $body, $sender)) { //mail() est une fonction php intégrée qui permet d'envoyer du courrier.
 				echo "Votre message a été envoyé";
 			} else {
@@ -25,5 +25,4 @@
 	} else {
 		echo "Les champs Email et message sont obligatoires !";
 	}
-
 ?>
